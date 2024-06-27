9689217e820f2d2a7047cd1947be7a542e72e2a2
Giovanni.iacono98 <98giovanniiacono98@gmail.com>
2024-06-27 15:58:51 +0200

    modifica url

--- pexels-start.js
+++ pexels-start.js
@@ -1,20 +1,27 @@
-fetch("https://api.pexels.com/v1/search?query=lifestyle", {
-  headers: {
-    "Content-Type": "application/json",
-    Authorization: "Doguu2nH5bVr1YOsFZLNrAqEOmlXGvEy3FqzkL5ryeTxIxahnq0glxlN",
-  },
-})
-  .then((resp) => {
-    console.log("ok ", resp);
-    if (resp.ok) {
-      return resp.json();
-    } else {
-      throw new err("errore: ", resp);
-    }
+const loadButtonElement = document.getElementById("loadButton");
+console.log(loadButtonElement);
+
+loadButtonElement.addEventListener("click", (event) => {
+  fetch("https://api.pexels.com/v1/curated?page=2&per_page=40", {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Doguu2nH5bVr1YOsFZLNrAqEOmlXGvEy3FqzkL5ryeTxIxahnq0glxlN",
+    },
   })
-  .then((lifestyle) => {
-    console.log(lifestyle);
-  })
-  .catch((err) => {
-    console.log("errore: ", err);
-  });
+    .then((resp) => {
+      //   console.log("ok ", resp);
+      if (resp.ok) {
+        return resp.json();
+      } else {
+        throw new Error("errore: ", resp);
+      }
+    })
+    .then((lifestyle) => {
+      lifestyle.forEach((element) => {
+        console.log(lifestyle.img);
+      });
+    })
+    .catch((err) => {
+      console.log("errore: ", err);
+    });
+});
